e78f7cfc0ec41b833ebed9421e4bc2decbde06a3
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-02-11 20:37:28 +0530

    Welcome To AddressBook

---
+++ src/com/addressbook/Person.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook");
+
+        Person contactPerson = new Person();
+    }
+}
